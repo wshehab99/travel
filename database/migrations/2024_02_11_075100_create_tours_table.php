77232173d2a8da785c16ed333971774a2a915585
Waleed Bin Shehab <waleedbinshihab@gmail.com>
2024-02-11 17:02:18 +0300

    migrations and models

--- database/migrations/2024_02_11_075100_create_tours_table.php
+++ database/migrations/2024_02_11_075100_create_tours_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tours', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignUuid('travel_id')->constrained('travels');
+            $table->timestamp('starting_date')->nullable();
+            $table->timestamp('ending_date')->nullable();
+            $table->unsignedInteger('price');
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tours');
     }
 };
